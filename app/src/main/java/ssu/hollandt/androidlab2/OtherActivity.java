7d5afebd709906c93a94409eb58f694d80c1e35c
student <student@iMac-103.local>
2018-08-30 21:05:31 -0700

    part 2 complete

--- app/src/main/java/ssu/hollandt/androidlab2/OtherActivity.java
+++ app/src/main/java/ssu/hollandt/androidlab2/OtherActivity.java
@@ -1,13 +1,40 @@
 package ssu.hollandt.androidlab2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class OtherActivity extends AppCompatActivity {
+
+    public static final String NAME_KEY = "USER _NAME";
+    private Button navigateBack;
+    private TextView welcomeText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_other);
+
+        Intent newIntent = getIntent();
+        String name = newIntent.getStringExtra(NAME_KEY);
+
+        if(name == null || name.isEmpty()){
+            name = "Not Bob";
+        }
+        welcomeText = findViewById(R.id.welcome_text);
+        welcomeText.setText("Welcome, " + name);
+
+        navigateBack = findViewById(R.id.back_button);
+        navigateBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(OtherActivity.this, MainActivity.class);
+
+                startActivity(intent);
+            }
+        });
     }
 }
