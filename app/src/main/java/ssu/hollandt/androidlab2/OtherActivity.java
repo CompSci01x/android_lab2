c3ae20b8a1f06a850fb7b3b666ceacc9e4f677af
student <student@iMac-5.local>
2018-08-30 18:44:42 -0700

    part 1 complete

--- app/src/main/java/ssu/hollandt/androidlab2/OtherActivity.java
+++ app/src/main/java/ssu/hollandt/androidlab2/OtherActivity.java
@@ -1,13 +1,49 @@
 package ssu.hollandt.androidlab2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class OtherActivity extends AppCompatActivity {
+
+    public static final String NAME_KEY = "USER_NAME";
+
+    private Button navigateToMain;
+    private TextView welcomeText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_other);
+
+
+
+        Intent newIntent = getIntent();
+        String name = newIntent.getStringExtra(NAME_KEY);
+
+
+
+
+        welcomeText=findViewById(R.id.welcome_text);
+        navigateToMain=findViewById(R.id.navigateToMain_button);
+        navigateToMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(OtherActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        if (name == null || name.isEmpty()) {
+            name = "Not Bob";
+        }
+        welcomeText.setText("Welcome, " + name);
+
     }
+
 }
