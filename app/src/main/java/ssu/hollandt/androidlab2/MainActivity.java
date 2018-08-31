266acf5ae72895d64f0f3efa08837fe3106752bc
Samuel Smetzer <kamuela.1300@gmail.com>
2018-08-30 18:17:29 -0700

    part 1 complete

--- app/src/main/java/ssu/hollandt/androidlab2/MainActivity.java
+++ app/src/main/java/ssu/hollandt/androidlab2/MainActivity.java
@@ -1,11 +1,16 @@
 package ssu.hollandt.androidlab2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     // class variables
+
+    private Button navigateButton;
 
 
     @Override
@@ -13,5 +18,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        navigateButton = findViewById(R.id.navigate_button);
+
+
+        navigateButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+
+            public void onClick(View view){
+
+                Intent intent = new Intent (MainActivity.this, OtherActivity.class);
+
+                startActivity(intent);
+            }
+        });
     }
 }
