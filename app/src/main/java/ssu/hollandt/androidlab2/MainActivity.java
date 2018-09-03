e9b184de383b87280ddcdc71ea85a73ffabf7813
student <student@ssu-cs-dar25-167-143.sonoma.edu>
2018-09-02 17:54:16 -0700

    Left off at message

--- app/src/main/java/ssu/hollandt/androidlab2/MainActivity.java
+++ app/src/main/java/ssu/hollandt/androidlab2/MainActivity.java
@@ -2,10 +2,12 @@ package ssu.hollandt.androidlab2;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    // class variables
+    private Button navigateButton;
 
 
     @Override
@@ -13,5 +15,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        navigateButton = (Button) findViewById(R.id.navigate_button);
+
+            navigateButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    //symbol class Intent does not exist
+                    Intent intent = new Intent(MainActivity.this, OtherActivity.class);
+                    startActivity(intent);
+                }
+            });
+
+        }
+
     }
-}
+
+
+
