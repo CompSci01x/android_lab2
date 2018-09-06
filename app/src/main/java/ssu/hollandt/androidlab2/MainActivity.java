0fc95c88c8de59aa45e2e5dfb76f80c1c918abc6
student <student@ssu-cs-dar28-167-201.sonoma.edu>
2018-09-06 12:09:46 -0700

    part 2 complete

--- app/src/main/java/ssu/hollandt/androidlab2/MainActivity.java
+++ app/src/main/java/ssu/hollandt/androidlab2/MainActivity.java
@@ -7,34 +7,38 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
     // class variables
     private Button navigateButton;
     private EditText nameEditText;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-     //assign  edit text value
-     nameEditText = findViewById(R.id.name_edit_text);
+        //assign  edit text value
+        nameEditText = findViewById(R.id.name_edit_text);
 
         //button that navigates from main activity to other activity
         navigateButton = findViewById(R.id.navigate_button);
-        navigateButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                String name = nameEditText.getText().toString();
-                Intent intent = new Intent(MainActivity.this, OtherActivity.class);
-                intent.putExtra(OtherActivity.NAME_KEY, name);
-                startActivity(intent);
-            }
-        });
 
-
-
-
+        navigateButton.setOnClickListener
+        (
+                new View.OnClickListener()
+                {
+                    @Override
+                    public void onClick(View v)
+                    {
+                        String name = nameEditText.getText().toString();
+                        Intent intent = new Intent(MainActivity.this, OtherActivity.class);
+                        intent.putExtra(OtherActivity.NAME_KEY, name);
+                        startActivity(intent);
+                    }
+                }
+        );
     }
 }
--- app/src/main/java/ssu/hollandt/androidlab2/OtherActivity.java
+++ app/src/main/java/ssu/hollandt/androidlab2/OtherActivity.java
@@ -3,29 +3,58 @@ package ssu.hollandt.androidlab2;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-public class OtherActivity extends AppCompatActivity {
+public class OtherActivity extends AppCompatActivity
+{
 
     public static final String NAME_KEY = "USER_NAME";
     private TextView welcomeText;
+    //step 11 part b, declare button variable
+    private Button otherActivityNavigationButton;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         //get intent used to begin this activity
-        Intent newIntent = getIntent();
+        final Intent newIntent = getIntent();
         //retrieve the VALUE stored in the EXTRAS using the same KEY
         //that we used to insert it in MainActivity
         String name = newIntent.getStringExtra(NAME_KEY);
 
         //protect ourselves from a user not entering a name
-        if (name == null || name.isEmpty()){
+        if (name == null || name.isEmpty())
             name = "Not Bob";
-        }
+
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_other);
+
+        //Step 10. put up a welcome text to display to show information passed by the intent
+        //need to both declare a welcome text variable at the top, and initialize with
+        //findViewById
         welcomeText = findViewById(R.id.welcome_text);
         welcomeText.setText("Welcome, " + name);
+
+
+        //Step 11 c , assign the button to the variable using findViewById
+        otherActivityNavigationButton = findViewById(R.id.other_activity_navigation_button);
+
+        otherActivityNavigationButton.setOnClickListener
+                (
+                        new View.OnClickListener()
+                        {
+                            @Override
+                            public void onClick(View view)
+                            {
+                                Intent intent = new Intent(OtherActivity.this, MainActivity.class);
+                                startActivity(intent);
+                            }
+                        }
+                );
     }
+
+
 }
