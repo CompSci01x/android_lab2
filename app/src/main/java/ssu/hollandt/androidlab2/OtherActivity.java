c81673d5bcc373bc0874a70757f09ce8c81c79e7
darinjbrown <browndar@sonoma.edu>
2018-09-12 16:06:09 -0700

    activity added

--- app/src/main/java/ssu/hollandt/androidlab2/OtherActivity.java
+++ app/src/main/java/ssu/hollandt/androidlab2/OtherActivity.java
@@ -2,12 +2,17 @@ package ssu.hollandt.androidlab2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class OtherActivity extends AppCompatActivity {
+
+    private Button navigateButon;
+    private TextView navigateText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.other_activity);
+        setContentView(R.layout.activity_other);
     }
 }
