81a4944c2a4170098fe726f8e9a82817ee68da99
darinjbrown <browndar@sonoma.edu>
2018-09-20 17:43:42 -0700

    Still working on 2nd Part

--- app/src/main/java/ssu/hollandt/androidlab2/MainActivity.java
+++ app/src/main/java/ssu/hollandt/androidlab2/MainActivity.java
@@ -5,24 +5,36 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
     // class variables
     private Button navigateButton;
+    private EditText nameEditText;
+
     // test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        navigateButton(R.id.navigate_button);
+        navigateButton.findViewById(R.id.navigate_button);
         navigateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,OtherActivity.class);
+                String name = nameEditText.getText().toString();
+                Intent intent = new Intent(MainActivity.this, OtherActivity.class);
+
+                intent.putExtra(OtherActivity.NAME_KEY, name);
                 startActivity(intent);
+                /*Intent intent = new Intent(MainActivity.this,OtherActivity.class);
+                startActivity(intent);*/
+
             }
         });
+        nameEditText.findViewById(R.id.name_edit_text);
 
 
     }
--- app/src/main/java/ssu/hollandt/androidlab2/OtherActivity.java
+++ app/src/main/java/ssu/hollandt/androidlab2/OtherActivity.java
@@ -1,5 +1,6 @@
 package ssu.hollandt.androidlab2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -9,10 +10,21 @@ public class OtherActivity extends AppCompatActivity {
 
     private Button navigateButon;
     private TextView navigateText;
+    private TextView welcomeText;
+
+    public static final String NAME_KEY = "USER_NAME";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_other);
+        Intent newIntent = getIntent();
+        String name = newIntent.getStringExtra(NAME_KEY);
+        if(name == null || name.isEmpty()) {
+            name = "Not Bob";
+        }
+        welcomeText.findViewById(R.id.welcome_text);
+
     }
 }
