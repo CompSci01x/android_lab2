6adaefa6dc4626db62eb007c7956e7c70fdfc2eb
darinjbrown <browndar@sonoma.edu>
2018-09-28 11:31:39 -0700

    part 2 of lab implemented but not working

--- app/src/main/java/ssu/hollandt/androidlab2/MainActivity.java
+++ app/src/main/java/ssu/hollandt/androidlab2/MainActivity.java
@@ -5,21 +5,30 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     // class variables
+    private EditText nameEditText;
+
+
     private Button navigateButton;
     // test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nameEditText = (EditText)findViewById(R.id.name_edit_text);
         navigateButton = (Button)findViewById(R.id.navigate_button);
         navigateButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View v) {
+                String name = nameEditText.getText().toString();
                 Intent intent = new Intent(MainActivity.this,OtherActivity.class);
+
+                intent.putExtra(OtherActivity.NAME_KEY, name);
                 startActivity(intent);
             }
         });
--- app/src/main/java/ssu/hollandt/androidlab2/OtherActivity.java
+++ app/src/main/java/ssu/hollandt/androidlab2/OtherActivity.java
@@ -5,10 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
+import android.view.View;
+import android.widget.*;
 
 public class OtherActivity extends AppCompatActivity {
 
     private Button navigateButon;
+    private Button returnToMainButton;
     private TextView navigateText;
     private TextView welcomeText;
 
@@ -25,6 +28,15 @@ public class OtherActivity extends AppCompatActivity {
             name = "Not Bob";
         }
         welcomeText.findViewById(R.id.welcome_text);
+        welcomeText.setText("Welcome, " + name);
+        returnToMainButton = findViewById(R.id.return_button);
+        returnToMainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(OtherActivity.this, MainActivity.class);
 
+                startActivity(intent);
+            }
+        });
     }
 }
