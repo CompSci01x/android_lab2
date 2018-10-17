ef92f4021f3b153a21a7d28cdaec2d3082e2a967
darinjbrown <browndar@sonoma.edu>
2018-10-17 08:23:06 -0700

    Lab 2 Complete

--- app/src/main/java/ssu/hollandt/androidlab2/MainActivity.java
+++ app/src/main/java/ssu/hollandt/androidlab2/MainActivity.java
@@ -5,12 +5,14 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     // class variables
     private Button navigateButton;
 
+    private EditText nameEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +22,15 @@ public class MainActivity extends AppCompatActivity {
         navigateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String name = nameEditText.getText().toString();
                 Intent intent = new Intent(MainActivity.this, OtherActivity.class);
+
+                intent.putExtra(OtherActivity.NAME_KEY, name);
                 startActivity(intent);
             }
+
         });
+        nameEditText = findViewById(R.id.name_edit_text);
 
     }
 }
